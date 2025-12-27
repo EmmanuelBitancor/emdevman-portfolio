1dc539ec69332e6dc154c53743c882a9f2cfdd66
Emmanuel S. Bitancor <126531641+EmmanuelBitancor@users.noreply.github.com>
2025-12-27 20:43:22 +0800

    Update next.config.ts

--- emdevman/next.config.ts
+++ emdevman/next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    qualities: [75, 85],
+    qualities: [75, 85], // This line is already correct, no change needed
   },
 };
 
